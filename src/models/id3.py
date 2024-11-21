d794de9bbf7d193dcbaff0ead1d6a68899520af0
DeltDev <deltenc88@gmail.com>
2024-11-21 09:58:37 +0700

    feat: ID3 WIP

--- src/models/id3.py
+++ src/models/id3.py
@@ -1,1 +1,22 @@
-# Kode ID3 taruh di sini
+class Node:
+    value = None
+    next = None
+    childs = None
+
+    def __init__(self):
+        self.value = None
+        self.next = None
+        self.childs = None
+
+class DecisionTreeClassifier:
+    def entropy(data):
+        pass
+
+    def informationGain(data):
+        pass
+
+    def splitInformation(data):
+        pass
+
+    def gainRatio(data):
+        pass
