043c8c1fbd529ea76c6514498e87108ccca21c82
DieroA <diero.purnama@gmail.com>
2024-12-11 18:34:49 +0700

    feat: import model knn di notebook

--- src/models/knn.py
+++ src/models/knn.py
@@ -1,8 +1,17 @@
-from typing import List, Dict
+from typing import List, Dict, Optional, Any, Union
 import pandas as pd
 import numpy as np
 
 class KNN:
+    def __init__(self, k: int, method: str = "manhattan", 
+                 p: Optional[int]  = None) -> None:
+        self.k: int                          = k
+        self.method: str                     = method
+        self.p: Optional[int]                = p
+
+        self.train_X: Optional[pd.DataFrame] = None
+        self.train_y: Optional[pd.Series]    = None
+
     @staticmethod
     def getNumericalFeatures(df: pd.DataFrame) -> pd.Index:
         return df.select_dtypes(include = ["float64", "int64", "int32"]).columns
@@ -13,8 +22,14 @@ class KNN:
         
         return (((train_X[numerical_features].sub(inp[numerical_features]).abs()) ** p).sum(axis = 1)) ** (1 / p)
     
-    @staticmethod
-    def run(train_X: pd.DataFrame, train_y: pd.Series, inp: pd.Series, k: int = 3, method: str = "manhattan", p: int = None) -> str:
+    def fit(self, X: pd.DataFrame, y: pd.Series):
+        self.train_X = X
+        self.train_y = y
+
+    def get_params(self, deep: bool = True) -> Dict[str, Any]:
+        return {"k": self.k, "method": self.method, "p": self.p}
+
+    def predict(self, input_X: Union[pd.DataFrame, pd.Series]) -> List[int]:
         '''
         Mencari klasifikasi dari data masukan menggunakan algoritma K-Nearest Neighbors (KNN). 
         Fungsi ini mengembalikan kelas yang paling sering muncul di antara K tetangga terdekat 
@@ -23,32 +38,40 @@ class KNN:
             2. Euclidean Distance (euclidean)
             3. Minkowski Distance (minkowski) - Memerlukan tambahan parameter p.
         '''
+        def classify_single(inp_row: pd.Series) -> str:
+            distances: pd.Series = None
+            if self.method == "manhattan":
+                distances = KNN.calcMinkowskiDist(self.train_X, inp_row, 1)  
+            elif self.method == "euclidean":
+                distances = KNN.calcMinkowskiDist(self.train_X, inp_row, 2)
+            elif self.method == "minkowski":
+                if self.p == None:
+                    raise ValueError("Nilai p kosong.")
+                distances = KNN.calcMinkowskiDist(self.train_X, inp_row, self.p)
+            else:
+                raise ValueError("Method tidak valid.")
 
-        distances: pd.Series = None
-        if method == "manhattan":
-            distances = KNN.calcMinkowskiDist(train_X, inp, 1)
-        elif method == "euclidean":
-            distances = KNN.calcMinkowskiDist(train_X, inp, 2)
-        elif method == "minkowski":
-            if p == None:
-                raise ValueError("Nilai p kosong.")
-            distances = KNN.calcMinkowskiDist(train_X, inp, p)
+            # Cari K tetangga dengan distance terkecil.
+            kNearestNeighbour: List[int] = []
+            while len(kNearestNeighbour) < self.k:
+                min_value_idx: int = np.argmin(distances)
+
+                distances: np.ndarray = np.delete(distances, min_value_idx)
+                kNearestNeighbour.append(min_value_idx)
+
+            # Cari kelas yang paling sering muncul di antara KNN. 
+            target: Dict[str, int] = {}
+            for idx in kNearestNeighbour:
+                neighbour: str = str(self.train_y.iloc[idx])
+                target[neighbour] = target.get(neighbour, 0) + 1
+            
+            # Kembalikan kelas yang paling sering muncul
+            most_common_class: str = max(target, key = target.get)
+            return int(most_common_class)
+
+        if isinstance(input_X, pd.Series):
+            return [classify_single(input_X)]
+        elif isinstance(input_X, pd.DataFrame):
+            return [classify_single(input_X.iloc[i]) for i in range(len(input_X))]
         else:
-            raise ValueError("Method tidak valid.")
-
-        # Cari K tetangga dengan distance terkecil.
-        kNearestNeighbour: List[int] = []
-        while len(kNearestNeighbour) < k:
-            min_value_idx: int = np.argmin(distances)
-
-            distances = np.delete(distances, min_value_idx)
-            kNearestNeighbour.append(min_value_idx)
-
-        # Cari kelas yang paling sering muncul di antara KNN. 
-        target: Dict[str, int] = {}
-        for idx in kNearestNeighbour:
-            neighbour: str = str(train_y.iloc[idx])
-            target[neighbour] = target.get(neighbour, 0) + 1
-        
-        # Kembalikan kelas yang paling sering muncul.
-        return max(target, key = target.get)
+            raise TypeError("Tipe input bukan merupakan pd.Series ataupun pd.DataFrame.")
