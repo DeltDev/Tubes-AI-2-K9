a85232b1228fc3aea2b92146574cbc5f6cd45cb2
DieroA <diero.purnama@gmail.com>
2024-12-12 08:36:59 +0700

    fix: benerin save load knn

--- src/models/knn.py
+++ src/models/knn.py
@@ -31,14 +31,14 @@ class KNN:
         return {"k": self.k, "method": self.method, "p": self.p}
 
     def predict(self, input_X: Union[pd.DataFrame, pd.Series]) -> List[int]:
-        '''
+        """
         Mencari klasifikasi dari data masukan menggunakan algoritma K-Nearest Neighbors (KNN). 
         Fungsi ini mengembalikan kelas yang paling sering muncul di antara K tetangga terdekat 
         berdasarkan jarak yang dihitung menggunakan salah satu dari metode berikut:
             1. Manhattan Distance (manhattan)
             2. Euclidean Distance (euclidean)
             3. Minkowski Distance (minkowski) - Memerlukan tambahan parameter p.
-        '''
+        """
         def classify_single(inp_row: pd.Series) -> str:
             distances: pd.Series = None
             if self.method == "manhattan":
@@ -64,12 +64,17 @@ class KNN:
             target: Dict[str, int] = {}
             for idx in kNearestNeighbour:
                 neighbour: str = str(self.train_y.iloc[idx])
-                target[neighbour] = target.get(neighbour, 0) + 1
-            
+                target[neighbour] = target.get(neighbour, 0) + 1     
+
             # Kembalikan kelas yang paling sering muncul
             most_common_class: str = max(target, key = target.get)
             return int(most_common_class)
 
+        # Cek model sudah di fit belum
+        if self.train_X is None or self.train_y is None:
+            raise ValueError("Model belum di fit.")
+
+        # Cari klasifikasi menggunakan KNN
         if isinstance(input_X, pd.Series):
             return [classify_single(input_X)]
         elif isinstance(input_X, pd.DataFrame):
@@ -77,27 +82,46 @@ class KNN:
         else:
             raise TypeError("Tipe input bukan merupakan pd.Series ataupun pd.DataFrame.")
         
-    def save(self, save_path: str) -> None:        
+    def save(self, save_path: str) -> None:     
+        """
+        Mengubah data model ke dalam bentuk json dan menyimpannya berdasarkan
+        path yang diberikan.
+        """
         serialized_model: Dict[str, Any] = {
             "k": self.k,
             "method": self.method,
             "p": self.p,
-            "train_X": self.train_X.to_dict(),
-            "train_y": self.train_y.to_list()
+            "train_X": None if self.train_X is None else self.train_X.to_dict(),
+            "train_y": None if self.train_y is None else self.train_y.to_list()
         }
 
         with open(save_path, "w") as file:
             json.dump(serialized_model, file)
 
-        print(f"Model saved to {save_path}")
+        print(f"Model tersimpan di {save_path}")
 
     @classmethod
     def load(cls, load_path: str) -> "KNN":
-        with open(load_path, "r") as file:
-            serialized_model: Dict[str, str] = json.load(file)
+        """
+        Membuka file yang berisi data model dan mengembalikan objek kelas KNN berdasarkan
+        data yang telah dimuat.
+        """
+        try:
+            with open(load_path, "r") as file:
+                serialized_model: Dict[str, str] = json.load(file)
 
-        knn = cls(k = serialized_model["k"], method = serialized_model["method"], p = serialized_model["p"])
-        knn.train_X = pd.DataFrame(serialized_model["train_X"])
-        knn.train_y = pd.DataFrame(serialized_model["train_y"])
+            knn: "KNN" = cls(
+                k = serialized_model["k"], 
+                method = serialized_model["method"], 
+                p = serialized_model["p"]
+            )
 
-        return knn
+            knn.train_X = None if serialized_model["train_X"] is None else pd.DataFrame(serialized_model["train_X"])
+            knn.train_y = None if serialized_model["train_y"] is None else pd.Series(serialized_model["train_y"])
+
+            print(f"Berhasil memuat model dari {load_path}")
+
+            return knn
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File {load_path} tidak ditemukan.")
+
